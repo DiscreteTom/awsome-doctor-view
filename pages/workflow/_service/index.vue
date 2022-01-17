3b4ed56b9e10550b05fd1b1a9b655ef6aee09a33
DiscreteTom <discrete_tom@outlook.com>
2022-01-17 22:35:29 +0800

    add service workflow list page

---
+++ pages/workflow/_service/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2 class="mx-3">Available workflows:</h2>
+    <v-list nav dense>
+      <v-list-item
+        v-for="(workflow, workflowName) in $store.state.workflows[service]"
+        :key="workflowName"
+        :to="`/workflow/${service}/${workflowName}`"
+      >
+        <v-list-item-icon>
+          <v-icon> mdi-link </v-icon>
+        </v-list-item-icon>
+        <v-list-item-title> {{ workflow.title }} </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const service = params.service;
+    return { service };
+  },
+};
+</script>
