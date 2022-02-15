85f2520ea21467fbd102a0ceb314e79df716a43b
DiscreteTom <discrete_tom@outlook.com>
2022-02-15 16:04:42 +0800

    use combobox to choose region

--- pages/index.vue
+++ pages/index.vue
@@ -13,10 +13,11 @@
       hide-details
       class="my-3"
     />
-    <v-text-field
+    <v-combobox
       v-model="region"
+      :items="Object.values(regionCodes)"
       label="Region Code"
-      placeholder="us-east-1"
+      placeholder="us-east-1: US East (N. Virginia)"
       hide-details
       class="my-3"
     />
@@ -53,7 +54,33 @@
 export default {
   data() {
     return {
-      region: "us-east-1",
+      region: "us-east-1: US East (N. Virginia)",
+      regionCodes: {
+        "us-east-2": "us-east-2: US East (Ohio)",
+        "us-east-1": "us-east-1: US East (N. Virginia)",
+        "us-west-1": "us-west-1: US West (N. California)",
+        "us-west-2": "us-west-2: US West (Oregon)",
+        "af-south-1": "af-south-1: Africa (Cape Town)",
+        "ap-east-1": "ap-east-1: Asia Pacific (Hong Kong)",
+        "ap-southeast-3": "ap-southeast-3: Asia Pacific (Jakarta)",
+        "ap-south-1": "ap-south-1: Asia Pacific (Mumbai)",
+        "ap-northeast-3": "ap-northeast-3: Asia Pacific (Osaka)",
+        "ap-northeast-2": "ap-northeast-2: Asia Pacific (Seoul)",
+        "ap-southeast-1": "ap-southeast-1: Asia Pacific (Singapore)",
+        "ap-southeast-2": "ap-southeast-2: Asia Pacific (Sydney)",
+        "ap-northeast-1": "ap-northeast-1: Asia Pacific (Tokyo)",
+        "ca-central-1": "ca-central-1: Canada (Central)",
+        "cn-north-1": "cn-north-1: China (Beijing)",
+        "cn-northwest-1": "cn-northwest-1: China (Ningxia)",
+        "eu-central-1": "eu-central-1: Europe (Frankfurt)",
+        "eu-west-1": "eu-west-1: Europe (Ireland)",
+        "eu-west-2": "eu-west-2: Europe (London)",
+        "eu-south-1": "eu-south-1: Europe (Milan)",
+        "eu-west-3": "eu-west-3: Europe (Paris)",
+        "eu-north-1": "eu-north-1: Europe (Stockholm)",
+        "me-south-1": "me-south-1: Middle East (Bahrain)",
+        "sa-east-1": "sa-east-1: South America (São Paulo)",
+      },
       tempAk: "",
       tempSk: "",
       persist: false,
@@ -62,7 +89,7 @@ export default {
   },
   methods: {
     restoreForm() {
-      this.region = this.$store.state.region;
+      this.region = this.regionCodes[this.$store.state.region];
       this.tempAk = this.$store.state.tempAk;
       this.tempSk = this.$store.state.tempSk;
       this.persist = this.$store.state.persistCredentials;
@@ -74,7 +101,7 @@ export default {
     },
     save() {
       this.$store.commit("updateConfig", {
-        region: this.region,
+        region: this.region.split(":")[0],
         tempAk: this.tempAk,
         tempSk: this.tempSk,
       });
